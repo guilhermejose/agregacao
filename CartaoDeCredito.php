b13d4a7803f3134dc86c48591795f1850e354c2f
php 03 <php.03@caverna.cec.ime.usp.br>
2018-01-24 21:37:48 -0200

    prim

--- CartaoDeCredito.php
+++ CartaoDeCredito.php
@@ -1,16 +1,37 @@
 <?php
+
+class Cliente
+{
+  public $Nome;
+  public $Codigo;
+}
+
 class CartaoDeCredito
 {
   public $numero;
   public $DataDeValidade;
-  public $Cliente;
+  public $Clien;
 }
 
-  $cart = new CartaoDeCredito;
-   $cart->numero=111111 ;
-   $cart->DataDeValidade="12/12/18";
+   $Clien = new Cliente();
+   $Cart  = new CartaoDeCredito();
 
-   echo "\nCartao : $cart->numero  \n \n"  ;
-   echo "Validade : $cart->DataDeValidade  \n \n"  ;
+
+   $Clien->Nome="Rafael Cosentino";
+   $Clien->Codigo=123;
+   $Cart->Clien=$Clien;
+
+
+   $Cart->numero=111111 ;
+   $Cart->DataDeValidade="12/12/18";
+
+   echo "\nCartao : $Cart->numero  \n\n"  ;
+   echo "Validade : $Cart->DataDeValidade  \n"  ;
+   echo "Referencia : {$Cart->Clien->Nome}  \n"  ;
+
+   echo "\nCliente : $Clien->Nome  \n\n"  ;
+   echo "Codigo : $Clien->Codigo  \n\n"  ;
+
+
 
 ?>
